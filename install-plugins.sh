652d29841fb30bd96667e2277d9ce905d9f55585
Jonathan Hurter <john@scalingo.com>
2018-08-27 18:30:16 +0200

    First test

---
+++ install-plugins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -e $GRAFANA_PLUGINS ] ; then
+  echo "No plugins to install"
+  exit 0
+fi
+
+OLD_IFS=$IFS
+IFS=','
+
+for plugin in $GRAFANA_PLUGINS ; do
+  echo "Installing $plugin"
+  grafana-cli plugins install $plugin
+done
+
+IFS=$OLD_IFS
